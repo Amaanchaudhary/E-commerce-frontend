062118290f83cec6e1da2d1aa43ee47b52632b65
Amaanchaudhary <chaudharyamaan982@gmail.com>
2024-04-29 22:38:57 +0530

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
+import Footer from './customer/components/Footer/Footer';
 import Navigation from './customer/components/Navigation/Navigations';
 import Homepage from './customer/pages/Homepage/Homepage';
 
 function App() {
   return (
     <div className="">
-      <Navigation/>
-       <div>
-          <Homepage/>
-       </div>
+      <Navigation />
+      <div>
+        <Homepage />
+      </div>
+      <Footer />
     </div>
   );
 }
--- frontend/src/customer/pages/Homepage/Homepage.jsx
+++ frontend/src/customer/pages/Homepage/Homepage.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import MainCarousel from '../../components/HomeCarousel/MainCarousel'
 import HomeSectionCarousel from '../../components/HomeSectionCarousel/HomeSectionCarousel'
+import { mens_kurta } from '../../../Data/Men/men_kurta'
 
 const Homepage = () => {
   return (
     <div>
         <MainCarousel/>
         <div className='space-y-10 py-20 flex-col justify-center px-5  lg:px-10'>
-            <HomeSectionCarousel/>
-            <HomeSectionCarousel/>
-            <HomeSectionCarousel/>
-            <HomeSectionCarousel/>
-            <HomeSectionCarousel/>
-            <HomeSectionCarousel/>
+            <HomeSectionCarousel data={mens_kurta} sectionName={"Men's Kurta"}/>
+            <HomeSectionCarousel data={mens_kurta} sectionName={"Men's Shoes"}/>
+            <HomeSectionCarousel data={mens_kurta} sectionName={"Men's Shirt"}/>
+            <HomeSectionCarousel data={mens_kurta} sectionName={"Women's Saree"}/>
+            <HomeSectionCarousel data={mens_kurta} sectionName={"Women's Dress"}/>
         </div>
     </div>
   )
