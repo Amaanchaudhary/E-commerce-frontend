fcb7f462163ab8be527f82f59aeef3bb2f2708a8
Amaanchaudhary <chaudharyamaan982@gmail.com>
2024-05-01 20:10:04 +0530

    updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,19 @@ import './App.css';
 import Footer from './customer/components/Footer/Footer';
 import Navigation from './customer/components/Navigation/Navigations';
 import Homepage from './customer/pages/Homepage/Homepage';
+import Products  from './customer/components/Product/Products' 
 
 function App() {
-  return (
-    <div className="">
-      <Navigation />
-      <div>
-        <Homepage />
+    return (
+      <div className="">
+        <Navigation />
+        <div>
+          {/* <Homepage /> */}
+          <Products />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
-  );
-}
+    );
+  }
 
 export default App;
