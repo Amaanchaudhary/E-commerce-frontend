991ca80ad2d406551865ca840d5a00cb9a61ba52
Amaanchaudhary <chaudharyamaan982@gmail.com>
2024-05-07 22:07:23 +0530

    updates

--- frontend/src/customer/components/AddressCard/AddressCard.jsx
+++ frontend/src/customer/components/AddressCard/AddressCard.jsx
@@ -7,7 +7,8 @@ const AddressCard = () => {
         <p className='font-semibold'>Amaan cahduahry</p>
         <p>mumbai , amrut nagr , thane , 400612 , amrut nagrar , 400612</p>
         <div className='space-y-1'>
-            <p className='font-semibold'>Phone NUmvber</p>
+            <p className='font-semibold'>Phone NUmvber</p> 
+            
             <p>9930368633</p>
         </div>
       </div>
