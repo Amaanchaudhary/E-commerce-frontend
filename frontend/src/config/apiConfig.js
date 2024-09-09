64fadc43e886064efd0200e4c9327afdbd0680f6
Amaanchaudhary <chaudharyamaan982@gmail.com>
2024-09-09 22:12:25 +0530

    1:36

--- frontend/src/config/apiConfig.js
+++ frontend/src/config/apiConfig.js
@@ -1,2 +1,12 @@
-export const BaseURL = "http://localhost:8000"
+import axios from "axios"
 
+export const BaseURL = "http://localhost:8000"
+const jwt = localStorage.getItem("jwt")
+
+export const api = axios.create({
+  baseURL: BaseURL,
+  headers: {
+    "Authorization": `Bearer ${jwt}`,
+    "Content-Type" : 'application/json'
+  }
+})
--- frontend/src/state/store.js
+++ frontend/src/state/store.js
@@ -1,9 +1,11 @@
 import { applyMiddleware, combineReducers, legacy_createStore } from 'redux'
 import { thunk } from 'redux-thunk'
 import { AuthReducer } from './Auth/Reducers'
+import { customerProductReducer } from './product/Reducer'
 
 const rootReducers = combineReducers({
-  auth : AuthReducer
+  auth : AuthReducer,
+  product: customerProductReducer
 })
 
 export const store = legacy_createStore(rootReducers, applyMiddleware(thunk))
