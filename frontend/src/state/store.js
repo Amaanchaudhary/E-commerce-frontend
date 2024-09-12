bd7716abd8217a53bbef0d4c17404930d7e9c6c2
Amaanchaudhary <chaudharyamaan982@gmail.com>
2024-09-12 21:58:45 +0530

    2:05

--- frontend/src/state/store.js
+++ frontend/src/state/store.js
@@ -3,11 +3,13 @@ import { thunk } from 'redux-thunk'
 import { AuthReducer } from './Auth/Reducers'
 import { customerProductReducer } from './product/Reducer'
 import { cartReducer } from './cart/Reducer'
+import { orderReducer } from './Order/Reducer'
 
 const rootReducers = combineReducers({
-  auth : AuthReducer,
+  auth: AuthReducer,
   product: customerProductReducer,
-  cart : cartReducer
+  cart: cartReducer,
+  order: orderReducer
 })
 
 export const store = legacy_createStore(rootReducers, applyMiddleware(thunk))
