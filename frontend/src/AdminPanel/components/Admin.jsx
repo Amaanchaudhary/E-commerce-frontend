8e763a98e1ce2ad5912f7f281bbe9cd256db422d
Amaanchaudhary <chaudharyamaan982@gmail.com>
2024-10-25 21:59:40 +0530

    Admin side Bar UI completed

--- frontend/src/AdminPanel/components/Admin.jsx
+++ frontend/src/AdminPanel/components/Admin.jsx
@@ -1,24 +1,39 @@
-import { useTheme } from "@emotion/react";
 import {
   Box,
+  CssBaseline,
+  Drawer,
   List,
   ListItem,
   ListItemButton,
   ListItemIcon,
+  ListItemText,
   Toolbar,
   useMediaQuery,
+  useTheme,
 } from "@mui/material";
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import EmailIcon from "@mui/icons-material/Email";
 import InboxIcon from "@mui/icons-material/Inbox";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import Dashboard from "./Dashboard";
+import CreateProducts from "./CreateProducts";
+import OrdersTable from "./OrdersTable";
+import CustomersTable from "./CustomersTable";
+import ProductTable from "./ProductTable";
+import AdminDashboard from "./Dashboard";
 
 const menu = [
-  { name: "Dashboard", path: "/admin" },
-  { name: "Products", path: "/admin/products" },
-  { name: "Customers", path: "/admin/customers" },
-  { name: "Orders", path: "/admin/orders" },
-  { name: "AddProduct", path: "/admin/product/create" },
+  { name: "Dashboard", path: "/admin", icon: <DashboardIcon /> },
+  { name: "Products", path: "/admin/products", icon: <DashboardIcon /> },
+  { name: "Customers", path: "/admin/customers", icon: <DashboardIcon /> },
+  { name: "Orders", path: "/admin/orders", icon: <DashboardIcon /> },
+  {
+    name: "AddProduct",
+    path: "/admin/product/create",
+    icon: <DashboardIcon />,
+  },
 ];
 const Admin = () => {
   const theme = useTheme();
@@ -33,9 +48,10 @@ const Admin = () => {
         display: "flex",
         flexDirection: "column",
         justifyContent: "space-between",
+        height: "100%",
       }}
     >
-      {isLargeScreen && <Toolbar />}
+      {/* {isLargeScreen && <Toolbar />} */}
       <List>
         {menu.map((item, index) => (
           <ListItem
@@ -44,17 +60,44 @@ const Admin = () => {
             onClick={() => navigate(item.path)}
           >
             <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <EmailIcon />}
-              </ListItemIcon>
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText>{item.name}</ListItemText>
             </ListItemButton>
           </ListItem>
         ))}
       </List>
+
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <AccountCircleIcon />
+            </ListItemIcon>
+            <ListItemText>Account</ListItemText>
+          </ListItemButton>
+        </ListItem>
+      </List>
     </Box>
   );
 
-  return <div>Admin</div>;
+  return (
+    <div>
+      <div className="flex h-[100vh]">
+        <CssBaseline />
+        <div className="w-[15%] border border-gray-300 h-full">{drawer}</div>
+
+        <div className="w-[85%] ">
+          <Routes>
+            <Route path="/" element={<AdminDashboard />} />
+            <Route path="/product/create" element={<CreateProducts />} />
+            <Route path="/products" element={<ProductTable />} />
+            <Route path="/orders" element={<OrdersTable />} />
+            <Route path="/customers" element={<CustomersTable />} />
+          </Routes>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Admin;
