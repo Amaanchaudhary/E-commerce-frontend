1eae5ffff067b9ef482e3447e9cff0691897e39e
Amaanchaudhary <chaudharyamaan982@gmail.com>
2025-01-19 12:32:02 +0530

    commit for frontend

--- frontend/src/config/apiConfig.js
+++ frontend/src/config/apiConfig.js
@@ -2,18 +2,15 @@ import axios from "axios"
 
 // export const BaseURL = "https://e-commerce-backend-ef1s.onrender.com"
 // export const BaseURL = "http://localhost:8000"
+
 export const BaseURL =
   process.env.NODE_ENV === "production"
     ? "https://e-commerce-backend-ashy-two.vercel.app"
     : "http://localhost:8000";
 
-
-// const jwt = localStorage.getItem("jwt")
-
 export const api = axios.create({
   baseURL: BaseURL,
   headers: {
-    // "Authorization": `Bearer ${jwt}`,
     "Content-Type": 'application/json'
   }
 })
@@ -31,3 +28,4 @@ api.interceptors.request.use(
     return Promise.reject(error);
   }
 );
+
